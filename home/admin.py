2ac404c51ccd87839afa07bda8d5066f5d5b3f20
AliKemalSahin <alikemalsahin78@gmail.com>
2020-04-29 18:17:29 +0300

    25.videoUyeProfilEkBilgi

--- home/admin.py
+++ home/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 
-from home.models import Setting, ContactFormMessage
+from home.models import Setting, ContactFormMessage, UserProfile
 
 
 class ContactFormMessageAdmin(admin.ModelAdmin):
     list_display = ['name', 'email', 'subject','message','note', 'status']
     list_filter = ['status']
 
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user_name', 'phone', 'adress', 'city', 'country', 'image_tag']
+
 admin.site.register(ContactFormMessage,ContactFormMessageAdmin)
 admin.site.register(Setting)
+admin.site.register(UserProfile, UserProfileAdmin)
--- home/models.py
+++ home/models.py
@@ -1,7 +1,10 @@
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.contrib.auth.models import User
 from django.db import models
 
 from django.forms import ModelForm, TextInput, Textarea
+from django.utils.safestring import mark_safe
+
 
 class Setting(models.Model):
     STATUS = (
@@ -66,10 +69,27 @@ class ContactFormu(ModelForm):
         }
 
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone = models.CharField(blank= True, max_length=20)
+    adress = models.CharField(blank= True, max_length=150)
+    city = models.CharField(blank= True, max_length=20)
+    country = models.CharField(blank= True, max_length=20)
+    image = models.ImageField(blank= True, upload_to='images/users/')
 
+    def __str__(self):
+        return self.user.username
+    def user_name(self):
+        return self.user.first_name + ' ' + self.user.last_name + '[' + self.user.username + '] '
 
+    def image_tag(self):
+        return mark_safe('<img src="{}" height="50"/>'.format(self.image.url))
+    image_tag.short_description = 'Image'
 
-
+class UserProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['phone', 'adress', 'city', 'country','image']
 
 
 
